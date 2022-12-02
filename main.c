173d764bac8f9c6b437d5cb268d90028e6362e06
Sinanmz <85231558+Sinanmz@users.noreply.github.com>
2022-12-02 13:18:20 +0330

    Revert "5th commit."
    This reverts commit ef56906b

--- main.c
+++ main.c
@@ -5,20 +5,9 @@ int main() {
     printf("Enter your name:\n");
     scanf("%s", s);
     printf("Hello %s\n", s);
-    printf("Enter 2 numbers:\n");
-    int a;
-    int b;
-    scanf("%d %d", &a, &b);
-    printf("GCD of those numbers is: %d", GCD(a, b));
-// a comment
+
 
 
 
     return 0;
-}
-    int GCD(int a, int b){
-    if(b == 0){
-        return a;
-    }
-    return GCD(b, a%b);
 }
